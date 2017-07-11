357453c80b5112e0b25867e1f5613b4bbd9ed051
wratterman <will.ratterman@gmail.com>
2017-07-11 11:47:16 -0600

    books#create complete and redirects to created book's show page. added link to books#index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,4 +11,17 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+
+    redirect_to book_path(@book)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :price, :quantity)
+  end
 end
