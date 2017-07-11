79eae423c7471e443cf1a693a91cf14d8df77060
wratterman <will.ratterman@gmail.com>
2017-07-11 11:48:36 -0600

    refactoring in books#create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,9 +14,11 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-
-    redirect_to book_path(@book)
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
   end
 
   private
